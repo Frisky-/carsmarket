82165c6effbe39e92f3ff7887e84375f74368f2e
Petar <test@test.com>
2016-05-04 20:36:22 +0300

    added filters for the ads on the main page

--- client/templates/home/index.js
+++ client/templates/home/index.js
@@ -1,16 +1,36 @@
+Session.set("type","");
+Session.set("condition","");
+Session.set("transmission","");
+Session.set("speeds","");
+
 Template.homeIndex.helpers({
   ads : function () {
-    console.log(Session.get("type"));
-    return Ads.find({
-      bodyType: Session.get("type")
-    });
+    var filters = {
+      bodyType:Session.get("type"),
+      condition: Session.get("condition"),
+      transmission: Session.get("transmission"),
+      speeds: Session.get("speeds")
+    };
+    var query = {};
+    for(var i in filters){
+      if(filters[i] !== ""){
+        query[i] = filters[i];
+      }
+    }
+      if (!jQuery.isEmptyObject(query)) {
+        return Ads.find({$and: [query]});
+      }else{
+        return Ads.find();
+      }
   }
 });
 
 Template.homeIndex.events({
-  'click .searchBtn' :function (e,t) {
-    e.preventDefault();
-    console.log($('.type').val());
-    Session.set("type", $('.type option:selected').text());
+  'change form' : function (event) {
+    event.preventDefault();
+    Session.set("type",$(".type").val());
+    Session.set("condition",$(".condition").val());
+    Session.set("transmission",$(".transmission").val());
+    Session.set("speeds",$(".speeds").val());
   }
-})
+});
